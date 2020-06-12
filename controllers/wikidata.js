003521d53f0e0ae482ce4df6cf10943a9f5a4145
Martin Schibel <mshd94@gmail.com>
2020-06-12 12:05:43 +0200

    first commit #1

---
+++ controllers/wikidata.js
@@ -0,0 +1,72 @@
+const wbk = require('wikidata-sdk');
+const fetch = require('node-fetch');
+
+exports.wikidataApi = function(para, callback) {
+    if(para.ids.length === 0){
+        callback(null,null);
+        return;
+    }
+    const urls = wbk.getManyEntities({
+        ids: para.ids,
+        languages: para.lang || [ 'en', 'fr', 'de' ], // returns all languages if not specified
+        props: para.props || [ 'labels', 'descriptions', 'claims', 'sitelinks/urls' ], // returns all data if not specified
+        // format: 'xml', // defaults to json
+        // redirections: false // defaults to true
+    });
+    console.log(urls);
+    if(urls.length === 1){
+        fetch(urls)
+            // .then(response => console.log(response))
+            .then(response => response.json())
+            // .then(wbk.parse.wd.entities)
+            .then(entities => {
+                // console.log(entities);
+                callback(null,entities);
+                // return entities;
+            })
+            // .catch(error =>{ console.log("ERROR fetch in Wikidata.js :"+error.message); callback(new Error("ERROR fetch in Wikidata.js :"+error.message),null); }); //add Error catch
+            //https://nodejs.org/api/errors.html#errors_error_first_callbacks
+    }else{
+        //https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all
+        //map all requests and emerge the entities
+        await Promise.all(urls.map(u=>fetch(u))).then(responses =>
+            Promise.all(responses.map(res => {
+                // console.log(res.);
+                return res.json();
+            }))
+        ).then(texts => {
+            var entities = {};
+            for(var text in texts){
+                for(var index in texts[text].entities){
+                    entities[index] = texts[text].entities[index];
+                }
+            }
+            callback(null,{entities: entities});
+
+        })
+            // .catch(error => {console.log("Error Promise in wikidata.js : "+error.message); callback(new Error("Error Promise in wikidata.js : "+error.message),null);}); //add Error catch
+    }
+
+};
+
+exports.wikidataSparql = function(para, callback) {
+    const url = wbk.sparqlQuery(para);
+    // callback(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(results => wbk.simplify.sparqlResults(results))
+        .then(entities => {
+            callback(entities);
+        });
+};
+
+exports.wikidataSparqlGetItems  = function(para, callback) {
+    exports.wikidataSparql(para,function (items) {
+        const ids = items.map(entry => entry.item.value);
+        exports.wikidataApi({
+            ids: ids,
+        },function(err,result) {
+            callback(result);
+        });
+    })
+};
---
+++ controllers/wikitree.js
@@ -0,0 +1,47 @@
+const wbk = require('wikidata-sdk');
+const fetch = require('node-fetch');
+const async = require('async');
+const moment = require('moment');
+const fs = require('fs');
+const NodeCache = require('node-cache');
+// var cache = require('memory-cache');
+// const wikidataLang = require('../public/js/wikidataLang');
+var wikidataController = require('../controllers/wikidata');
+var processNode = require('../controllers/processNode');
+var treeType;
+var stackChildren = true;
+var dataCache = new NodeCache();
+var config = require('../controllers/tables');
+
+exports.init = function (request, callback) {
+    //from the debugging, it's seems that the data is append to the rows, so we need to initialized it for every request
+    //Init container data
+    rows =[];
+    labelIds = [];
+    processNode.ownValue = [];
+    processNode.nodeImages = [];
+    processNode.birthAndDeathPlace = [];
+    processNode.result.root = null;
+    if(!request.page){
+        callback('no page req');
+    }
+    var requestedTable = config.page[request.page];
+    var sparql = requestedTable.query;
+    var table = {};
+    wikidataController.wikidataSparqlGetItems(sparql,function (result) {
+        // callback(null,result);
+        console.log(result);
+        for(var entityId in result.entities){
+            const claims = wbk.simplify.claims(result.entities[entityId], { keepQualifiers: true });
+            table[entityId] = {};
+            requestedTable.header.forEach(function (headItem) {
+                table[entityId][headItem.name] = "test";
+            });
+
+        }
+        callback(null,table);
+
+        // callback(null,result);
+    })
+
+};
